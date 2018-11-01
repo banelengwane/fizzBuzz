659b69f868ab9e9abff51d783d070f24076ddde3
banelengwane <banelengwane@gmail.com>
2018-11-01 17:58:17 +0200

    cleared commented code

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,32 +1,23 @@
 module.exports = function (numbers){
-    // if( typeof num === 'number' || num instanceof Number){
-    //     if ((num%3 === 0) && (num%5 === 0)){
-    //         return "FizzBuzz";
-    //     } else if (num%3 === 0){
-    //         return "Fizz";
-    //     }else if (num%5 === 0){
-    //         return "Buzz";
-    //     }else{
-    //         return num;
-    //     }
-    // }else{
-    //     return 'not a number';
-    // }
-    let results = [];
-    let i = 1
-    while(i <= numbers){
-        if((i%3 === 0) && (i%5 === 0)){
-            results.push('FizzBuzz');
-        } else if (i%3 === 0){
-            results.push('Fizz');
-        } else if (i%5 === 0){
-            results.push('Buzz');
-        } else{
-            results.push(i);
-        }
-        i++;
+    if( typeof numbers === 'number' || numbers instanceof Number){
+        let results = [];
+        let i = 1
+        
+        while(i <= numbers){
+            if((i%3 === 0) && (i%5 === 0)){
+                results.push('FizzBuzz');
+            } else if (i%3 === 0){
+                results.push('Fizz');
+            } else if (i%5 === 0){
+                results.push('Buzz');
+            } else{
+                results.push(i);
+            }
+            i++;
+        }   
+        return results;
+
+    }else{
+        return 'not a number';
     }
-
-    return results;
-
 }
